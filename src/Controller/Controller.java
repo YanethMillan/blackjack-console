fbc02f94255d81222007b36c05eabec77830cb26
Yaneth Millan <yanethmillanalcocer@gmail.com>
2026-02-23 00:09:05 -0600

    Backend complete: blackjack game flow with hit/stand, machine logic and winner

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,23 +1,101 @@
 package Controller;
-import model.Deck;
-import model.Card;
-import view.GraphicEngine;
 
-import java.util.ArrayList;
+import model.Deck;
+import model.Hand;
+import java.util.Scanner;
 
 public class Controller {
     public static void main(String[] args) {
 
         Deck deck = new Deck();
-        GraphicEngine graphicEngine = new GraphicEngine();
-
         deck.fillDeck();
         deck.shuffle();
-        ArrayList<Card> cards = deck.getCards();
 
-        //Este metodo puede ser comentado, solo lo puse para imprimir las cartas
-        graphicEngine.printHand(cards);
+        Hand playerHand = new Hand();
+        Hand machineHand = new Hand();
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Welcome to Blackjack 21");
+
+        playerHand.addCard(deck.giveCard());
+        playerHand.addCard(deck.giveCard());
+
+        machineHand.addCard(deck.giveCard());
+        machineHand.addCard(deck.giveCard());
+
+        System.out.println("Your starting points: " + playerHand.sumScore());
+
+        boolean playerTurn = true;
+        boolean gameOver = false;
+
+        while (playerTurn) {
+
+            System.out.println("Do you want to Hit or Stand?");
+            String option = scanner.nextLine();
+
+            if (option.equals("Hit")) {
+
+                playerHand.addCard(deck.giveCard());
+                System.out.println("Your current points: " + playerHand.sumScore());
+
+                if (playerHand.bust()) {
+                    System.out.println("You lose!");
+                    gameOver = true;
+                    playerTurn = false;
+                }
+            }
+            else if (option.equals("hit")) {
+
+                playerHand.addCard(deck.giveCard());
+                System.out.println("Your current points: " + playerHand.sumScore());
+
+                if (playerHand.bust()) {
+                    System.out.println("You lose!");
+                    gameOver = true;
+                    playerTurn = false;
+                }
+            }
+            else if (option.equals("Stand")) {
+                playerTurn = false;
+            }
+            else if (option.equals("stand")) {
+                playerTurn = false;
+            }
+            else {
+                System.out.println("Invalid option. Please type Hit or Stand.");
+            }
+        }
+
+        if (gameOver) {
+            return;
+        }
+
+        System.out.println("T-1000 is playing...");
+
+        int machinePoints = machineHand.sumScore();
+
+        while (machinePoints < 17) {
+            machineHand.addCard(deck.giveCard());
+            machinePoints = machineHand.sumScore();
+        }
+
+        System.out.println("T-1000 points: " + machineHand.sumScore());
+        
+        int playerScore = playerHand.sumScore();
+        int finalMachineScore = machineHand.sumScore();
+
+        if (machineHand.bust()) {
+            System.out.println("T-1000 busted. You win!");
+        }
+        else if (playerScore > finalMachineScore) {
+            System.out.println("You win!");
+        }
+        else if (playerScore < finalMachineScore) {
+            System.out.println("You lose against T-1000.");
+        }
+        else {
+            System.out.println("It's a tie.");
+        }
     }
 }
--- src/model/Hand.java
+++ src/model/Hand.java
@@ -2,4 +2,90 @@ package model;
 import java.util.ArrayList;
 
 public class Hand {
+    private ArrayList<Card> cards;
+
+    public Hand() {
+        cards = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int sumScore() {
+        int sum = 0;
+
+        for (int i = 0; i < cards.size(); i++) {
+            Card card = cards.get(i);
+            String rank = card.getRank();
+
+            if (rank.equals("J")) {
+                sum = sum + 10;
+            }
+            else if (rank.equals("Q")) {
+                sum = sum + 10;
+            }
+            else if (rank.equals("K")) {
+                sum = sum + 10;
+            }
+            else if (rank.equals("A")) {
+                sum = sum + 11;
+            }
+            else if (rank.equals("2")) {
+                sum = sum + 2;
+            }
+            else if (rank.equals("3")) {
+                sum = sum + 3;
+            }
+            else if (rank.equals("4")) {
+                sum = sum + 4;
+            }
+            else if (rank.equals("5")) {
+                sum = sum + 5;
+            }
+            else if (rank.equals("6")) {
+                sum = sum + 6;
+            }
+            else if (rank.equals("7")) {
+                sum = sum + 7;
+            }
+            else if (rank.equals("8")) {
+                sum = sum + 8;
+            }
+            else if (rank.equals("9")) {
+                sum = sum + 9;
+            }
+            else if (rank.equals("10")) {
+                sum = sum + 10;
+            }
+        }
+        return sum;
+    }
+
+    public boolean bust(){
+        return sumScore() > 21;
+    }
 }
+
+
+
+
+
+        /*
+        tiene una lista de cartas
+
+        método addCard(carta):
+        agregar la carta a la lista
+
+        método sumScore():
+        recorrer todas las cartas
+        convertir cada carta a puntos de blackjack
+        sumar esos puntos
+        regresar la suma
+
+        método bust():
+        si sumScore() > 21
+        regresar true
+        si no
+        regresar false
+        */
